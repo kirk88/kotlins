19c07a47cbfff211b4de95b40e60db8312028ba6
Kirk <481314821@qq.com>
2021-04-18 21:28:47 +0800

    performance improvements

--- common-ktx/src/main/java/com/nice/kotlins/widget/TitleAppBar.kt
+++ common-ktx/src/main/java/com/nice/kotlins/widget/TitleAppBar.kt
@@ -11,7 +11,6 @@ import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Build
 import android.util.AttributeSet
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
