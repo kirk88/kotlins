cac845ca381180b4fc2ccf85912e04f7a56bfb80
Kirk <481314821@qq.com>
2021-04-30 21:51:23 +0800

    performance improvements

---
+++ common-ktx/src/main/java/com/nice/kotlins/helper/EditText.kt
@@ -0,0 +1,19 @@
+package com.nice.kotlins.helper
+
+import android.widget.EditText
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+fun EditText.showIme(){
+    requestFocus()
+    post {
+        ViewCompat.getWindowInsetsController(this)?.show(WindowInsetsCompat.Type.ime())
+    }
+}
+
+fun EditText.hideIme(){
+    clearFocus()
+    post {
+        ViewCompat.getWindowInsetsController(this)?.hide(WindowInsetsCompat.Type.ime())
+    }
+}
