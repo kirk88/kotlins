f1a31b283ee5c191ead6c44e66d704a18b994011
lizhanping <1760316362@qq.com>
2021-07-20 16:57:19 +0800

    performance improvements

--- common/src/main/kotlin/com/nice/common/CommonInitializer.kt
+++ common/src/main/kotlin/com/nice/common/CommonInitializer.kt
@@ -7,16 +7,17 @@ import androidx.startup.Initializer
 import com.nice.common.app.ScreenAdaptation
 import com.nice.common.helper.application
 
-object Kotlins
+object Common
 
-class KotlinsInitializer: Initializer<Kotlins> {
-    override fun create(context: Context): Kotlins {
+class CommonInitializer: Initializer<Common> {
+
+    override fun create(context: Context): Common {
         val application = requireNotNull(context.application) {
             "Can not get application from context $context"
         }
         ApplicationContextHolder.init(application)
         ScreenAdaptation.init(application)
-        return Kotlins
+        return Common
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
