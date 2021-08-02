759afec72ceed52e8a7507a39365306d1e917e53
lizhanping <1760316362@qq.com>
2021-08-02 16:55:38 +0800

    performance improvements

--- sqlite/src/main/kotlin/com/nice/sqlite/core/ddl/Assignment.kt
+++ sqlite/src/main/kotlin/com/nice/sqlite/core/ddl/Assignment.kt
@@ -3,6 +3,7 @@ package com.nice.sqlite.core.ddl
 import com.nice.sqlite.core.dml.MutableSequence
 import com.nice.sqlite.core.dml.OnceIterator
 import com.nice.sqlite.core.dml.mutableSequenceOf
+import com.nice.sqlite.core.render
 import com.nice.sqlite.core.toSqlString
 
 interface Assignment : Sequence<Assignment>, Renderer {
@@ -15,11 +16,19 @@ interface Assignment : Sequence<Assignment>, Renderer {
     operator fun plus(assignment: Assignment): MutableSequence<Assignment> =
         mutableSequenceOf(this, assignment)
 
-    override fun render(): String =
-        "${column.render()} = ${value.toSqlString()}"
+    override fun render(): String = buildString {
+        append(column.name.render())
+        append(" = ")
+        append(value.toSqlString())
+    }
 
-    override fun fullRender(): String =
-        "${column.fullRender()} = ${value.toSqlString()}"
+    override fun fullRender(): String = buildString {
+        append(column.table.render())
+        append('.')
+        append(column.name.render())
+        append(" = ")
+        append(value.toSqlString())
+    }
 
     class Value(override val column: Column<*>, override val value: Any?) : Assignment {
         override fun toString(): String = "$column = $value"
--- sqlite/src/main/kotlin/com/nice/sqlite/core/ddl/Ordering.kt
+++ sqlite/src/main/kotlin/com/nice/sqlite/core/ddl/Ordering.kt
@@ -5,6 +5,7 @@ package com.nice.sqlite.core.ddl
 import com.nice.sqlite.core.dml.MutableSequence
 import com.nice.sqlite.core.dml.OnceIterator
 import com.nice.sqlite.core.dml.mutableSequenceOf
+import com.nice.sqlite.core.render
 
 enum class SqlOrderDirection { ASC, DESC }
 
@@ -18,9 +19,19 @@ interface Ordering : Sequence<Ordering>, Renderer {
     operator fun plus(ordering: Ordering): MutableSequence<Ordering> =
         mutableSequenceOf(this, ordering)
 
-    override fun render(): String = "${column.render()} $direction"
+    override fun render(): String = buildString {
+        append(column.name.render())
+        append(' ')
+        append(direction)
+    }
 
-    override fun fullRender(): String = "${column.fullRender()} $direction"
+    override fun fullRender(): String = buildString {
+        append(column.table.render())
+        append('.')
+        append(column.name.render())
+        append(' ')
+        append(direction)
+    }
 
     class By(override val column: Column<*>, override val direction: SqlOrderDirection) :
         Ordering {
