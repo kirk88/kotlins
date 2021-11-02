9e9487bcba8d7883667133090a22419bcf8af7de
kirk88 <481314821@qq.com>
2021-11-02 21:58:35 +0800

    Optimize code

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,8 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "7.2.0-alpha03"
-        id("com.android.library") version "7.2.0-alpha03"
+        id("com.android.application") version "7.0.3"
+        id("com.android.library") version "7.0.3"
         id("org.jetbrains.kotlin.android") version "1.5.31"
         id("org.jetbrains.kotlin.jvm") version "1.5.31"
         id("com.github.ben-manes.versions") version "0.39.0"
