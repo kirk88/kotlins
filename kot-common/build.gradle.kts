6798677f350636381c930a930af3218927d5907f
kirk88 <481314821@qq.com>
2021-12-14 16:32:03 +0800

    Optimize code

--- kot-common/build.gradle.kts
+++ kot-common/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
 val versionMajor = 1
 val versionMinor = 2
 val versionPatch = 0
-val versionSuffix = "alpha11"
+val versionSuffix = "alpha12"
 
 val sourcesJar by tasks.creating(Jar::class) {
     archiveClassifier.set("sources")
